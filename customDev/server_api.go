80faf9127b6c3e2f1ebc1f5552fba0a96bfdf6f7
Covid19sp <covid19sp@protonmail.com>
2021-03-16 04:24:49 +0900

    action

--- customDev/server_api.go
+++ customDev/server_api.go
@@ -16,7 +16,8 @@ func ApiWebServer() {
 // Set Routes
 func setupRoutes(app *fiber.App) {
 	// set handler for index page
-	app.Get("/api/freePort", GetFreePort) // set handler for index page
+	app.Get("/api/freePort", GetFreePort)      // set handler for index page
+	app.Get("/api/randHttpProxy", GetFreePort) // set handler for index page
 }
 
 // 返回一个空闲可用端口, 注意防火墙开启端口
@@ -24,4 +25,10 @@ func GetFreePort(c *fiber.Ctx) error {
 	availablePort := strconv.Itoa(FindFreePort())
 
 	return c.SendString(availablePort)
+}
+
+// 返回代理
+func RandHttpProxy(c *fiber.Ctx) error {
+
+	return c.SendString("ffff")
 }
